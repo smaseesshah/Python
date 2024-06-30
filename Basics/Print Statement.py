869e45b0f65ee4813ab92db052d827aaefec987a
smaseesshah <smaseesshah786@gmail.com>
2024-06-30 16:53:44 +0500

    Day3+Day4+Day5

---
+++ Basics/Print Statement.py
@@ -0,0 +1,5 @@
+#Print Statement
+print("asees",1,1.7)#consist of objects
+print("Separate Each By :) ","Asees","Muhammad",sep=":) ")#Separator (by default " ")
+print("End line By end and newline","Asees","Shah",end=" end\n")#separator (by default \n)
+#file + flush will be studied later
--- Basics/lets start.py
+++ Basics/lets start.py
@@ -1,2 +1,3 @@
 print('hello world')
 print(7)
+print("Syed Muhammad Asees SHah",149)
