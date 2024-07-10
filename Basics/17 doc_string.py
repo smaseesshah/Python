43c89c4257a279708e2faf1112a95cb2de725839
smaseesshah <smaseesshah786@gmail.com>
2024-07-10 16:23:27 +0500

    -Day28-29-30

---
+++ Basics/17 doc_string.py
@@ -0,0 +1,5 @@
+def doc_string():
+    '''This is a function which is just a doc string and return 0 only'''
+    return 0
+print("Function Calling",doc_string())
+print("Doc_String Calling",doc_string.__doc__)
